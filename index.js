5fb91376a6f62746d428fdd74093854ddb7e4a53
JUAN GOMEZ <juandiego.gomez1@utp.edu.co>
2025-08-02 14:43:59 -0500

    init project express

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+const upload = multer()
+
+const allowedImageTypes = [
+  'image/jpeg',
+  'image/png',
+  'image/webp',
+  'image/gif',
+  'image/tiff',
+  'image/bmp',
+  'image/avif'
+];
+
+
+
+app.get('/', (req, res) => {
+  res.send('¡Hola, mundo!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});
